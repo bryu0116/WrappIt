7ee99e47d52ab4d1f7b27116881994d1f695d404
bryu0116 <byenguk0116@gmail.com>
2020-12-05 13:45:36 -0500

    Adding route code

--- server.js
+++ server.js
@@ -26,10 +26,11 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-// Must change depends on Routes setup.
-//var routes = require("./controllers/wrappit_controller.js");
+//Must change depends on Routes setup.
+var routes = require("./controllers/wrappit_controller.js");
 
-//app.use(routes);
+app.use(routes);
+
 
 db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
